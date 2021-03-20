e2c9c8ded240a38fe57d5f0359223958975f443e
Roy Ratcliffe <royratcliffe@me.com>
2021-03-20 17:55:23 +0000

    Roxygen style

--- R/sqlformat.R
+++ R/sqlformat.R
@@ -1,12 +1,17 @@
-# Formats SQL using the sqlformat.org API. See example usage below. Arguments
-# are named list elements corresponding to format parameters. Only "sql" is
-# required; the rest have sensible defaults.
-#
-#   > sql.formatter(sql="select * from foo", reindent=1, keyword_case="upper")
-#   [1] "SELECT *\nFROM foo"
-#
-# See https://sqlformat.org/api/ for details. A usage limit exists: 500 requests
-# per hour based on the IP address.
+#' Formats SQL using the sqlformat.org API.
+#'
+#' @description See example usage below. Arguments
+#' are named list elements corresponding to format parameters. Only `sql` is
+#' required; the rest have sensible defaults.
+#'
+#' See https://sqlformat.org/api/ for details. A usage limit exists: 500 requests
+#' per hour based on the IP address.
+#'
+#' @param sql Structured Query Language.
+#' @param ... Format parameters.
+#' @examples
+#' sql.format(sql="select * from foo", reindent=1, keyword_case="upper")
+#' # -> [1] "SELECT *\nFROM foo"
 sql.format <- function(sql, ...) {
   response <- httr::POST("https://sqlformat.org/api/v1/format",
     body = list(sql = sql, ...))
