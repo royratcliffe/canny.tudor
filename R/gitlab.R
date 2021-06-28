796259699688f2d964bda979ebdb2c05b1f9d9cb
Roy Ratcliffe <royratcliffe@me.com>
2021-06-28 19:22:28 +0100

    GitLab functions

---
+++ R/gitlab.R
@@ -0,0 +1,57 @@
+#' Sets up a connection to gitlab.com using GITLAB_PRIVATE_TOKEN
+#'
+#' @export
+set.gitlab.connection <- function(...)
+  gitlabr::set_gitlab_connection(
+    gitlabr::gl_connection("https://gitlab.com",
+                           Sys.getenv("GITLAB_PRIVATE_TOKEN"), ...))
+
+#' Lists maintainer-level projects using default GitLab connection
+#'
+#' @export
+list.gitlab.maintainer.projects <- function(...)
+  gitlabr::gl_list_projects(..., min_access_level = 40)
+
+#' Lists owner-level projects using default GitLab connection
+#'
+#' @export
+list.gitlab.owner.projects <- function(...)
+  gitlabr::gl_list_projects(..., min_access_level = 50)
+
+#' Gets project identifier as integer
+#'
+#' You can use the extra arguments to filter the incoming project tibble by path
+#' or other criteria.
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#' list.gitlab.maintainer.projects() %>%
+#'   get.gitlab.project.id(path == "path-of-gitlab-project")
+#' }
+get.gitlab.project.id <- function(...)
+  dplyr::filter(...) %>%
+  dplyr::select(id) %>%
+  dplyr::mutate(dplyr::across(id, as.integer))
+
+#' Lists tree recursively
+#'
+#' Runs iteratively for all tree-type files. The tree path becomes a new path
+#' for a sub-listing.
+#'
+#' @importFrom magrittr %>%
+#' @param ... Arguments for gl_list_files
+#' @export
+list.gitlab.project.files <- function(...) {
+  files <- gitlabr::gl_list_files(...)
+  trees <- files[files$type == "tree", ]
+  while (length(trees) != 0) {
+    rows <- lapply(trees$path,
+                   function(x)
+                     gitlabr::gl_list_files(..., path = x)) %>%
+      dplyr::bind_rows()
+    files <- dplyr::add_row(files, rows)
+    trees <- rows[rows$type == "tree", ]
+  }
+  files
+}
