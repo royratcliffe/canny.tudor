f3b01d4d1447e83c9f1799ea672806c7065ebd1a
Roy Ratcliffe <royratcliffe@me.com>
2021-07-09 18:14:36 +0100

    docker_system_info, etc.

---
+++ R/docker.R
@@ -0,0 +1,25 @@
+#' Home directory's docker configuration
+#'
+#' Begins with full-stop (dot) in order not to overlap "docker config" as a
+#' command. The latter manages Docker configurations, while this function reads
+#' only the calling user's configuration file.
+#' @export
+.docker_config <- function()
+  jsonlite::fromJSON(file(fs::path_home(".docker", "config.json")))
+
+#' System-wide Docker information
+#'
+#' @return List of system-wide information pairs. There are some 60 pairs, typically.
+#' @export
+docker_system_info <- function() docker("system", "info")
+
+#' Docker information
+#'
+#' Synonym for Docker \emph{system} information.
+#' @export
+docker_info <- function() docker("info")
+
+#' @export
+docker <- function(...)
+  jsonlite::fromJSON(
+    system2("docker", args = c(..., "-f", '"{{json .}}"'), stdout = TRUE))
