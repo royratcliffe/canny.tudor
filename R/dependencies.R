203df7bf7119368c070b87c13d32225075c5a7f2
Roy Ratcliffe <royratcliffe@me.com>
2021-08-09 17:15:17 +0100

    Add examples

--- R/dependencies.R
+++ R/dependencies.R
@@ -5,5 +5,18 @@
 #' @inheritDotParams gtools::getDependencies
 #' @return List of \code{\link[R.oo]{Package}} dependencies.
 #' @export
+#' @examples
+#' library(R.oo)
+#' methods(class = "Package")
+#' #  [1] as.character        getAuthor           getBundle           getBundlePackages
+#' #  [5] getChangeLog        getClasses          getContents         getContribUrl
+#' #  [9] getDataPath         getDate             getDescription      getDescriptionFile
+#' # [13] getDevelUrl         getDocPath          getEnvironment      getExamplePath
+#' # [17] getHistory          getHowToCite        getLicense          getMaintainer
+#' # [21] getName             getNews             getPath             getPosition
+#' # [25] getTitle            getUrl              getVersion          isLoaded
+#' # [29] isOlderThan         ll                  load                showChangeLog
+#' # [33] showContents        showDescriptionFile showHistory         showHowToCite
+#' # [37] showNews            startupMessage      unload
 dependentPackages <- function(...)
   Map(R.oo::Package, gtools::getDependencies(...))
