af41f2dae0f339ea4d24b75ba6d157e75ab6d6e5
Roy Ratcliffe <royratcliffe@me.com>
2021-09-01 18:52:43 +0100

    strsplit.trimws function

---
+++ R/base.R
@@ -0,0 +1,12 @@
+#' Split string and trim white space
+#'
+#' Trims both leading and trailing white space.
+#'
+#' @param x What to split, a character vector
+#' @param split What to use for splitting. Either a character vector or an
+#'   object that can coerce to such.
+#' @inheritDotParams base::strsplit
+#' @return Split strings, a character vector
+#' @export
+strsplit.trimws <- function(x, split, ...)
+  unlist(lapply(strsplit(x, split, ...), trimws))
