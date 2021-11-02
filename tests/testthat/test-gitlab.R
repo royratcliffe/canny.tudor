d62ae6a518c18bdd53f22d861a5baa8a970bc51c
Roy Ratcliffe <royratcliffe@me.com>
2021-11-02 05:45:05 +0000

    Test connection set up

--- tests/testthat/test-gitlab.R
+++ tests/testthat/test-gitlab.R
@@ -1,6 +1,4 @@
 test_that("GitLab works", {
-  library(gitlabr)
-  set_gitlab_connection(gitlab_url = "https://gitlab.com", private_token = "")
-
+  expect_type(set.gitlab.connection(), "closure")
   expect_snapshot(list.gitlab.project.files("30946364"))
 })
