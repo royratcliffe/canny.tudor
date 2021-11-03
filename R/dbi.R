ea15ccc12820491f533632ccaa521ab27f239f99
Roy Ratcliffe <royratcliffe@me.com>
2021-11-03 19:58:15 +0000

    DBI schema

---
+++ R/dbi.R
@@ -0,0 +1,33 @@
+#' Define schema
+#' @param x Name of schema
+#' @export
+schema <- function(x) structure(x, class = "schema")
+
+#' Glue SQL schema
+#' @param x Schema to use for gluing
+#' @param ... Extra arguments passed to method implementation
+#' @export
+glue_sql <- function(x, ...) UseMethod("glue_sql", x)
+
+#' Glue schema to SQL statement
+#' @param x Schema to use for gluing
+#' @inheritDotParams glue::glue_sql
+#' @export
+glue_sql.schema <- function(x, ...) glue::glue_sql(..., schema = x)
+
+#' Create scheme unless exists
+#' @param x Schema to create
+#' @param ... Extra arguments passed to method implementation
+#' @export
+create_if_not_exists <- function(x, ...) UseMethod("create_if_not_exists", x)
+
+#' Create scheme if it does not yet exist
+#' @param x Schema to create, if not existing
+#' @param .con Database connect to use for literal quoting and for executing the
+#'   schema-glued statement
+#' @inheritDotParams DBI::dbExecute
+#' @export
+create_if_not_exists.schema <- function(x, .con = DBI::ANSI(), ...)
+  DBI::dbExecute(.con,
+                 glue_sql(x, "CREATE SCHEMA IF NOT EXISTS {`schema`}", .con),
+                 ...)
