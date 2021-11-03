9a38eaa6ea43f632be392856d5dfe9f6adafc805
Roy Ratcliffe <royratcliffe@me.com>
2021-11-03 20:14:46 +0000

    Needs export

--- R/dbi.R
+++ R/dbi.R
@@ -12,6 +12,7 @@ glue_sql <- function(x, ...) UseMethod("glue_sql", x)
 #' Glue schema to SQL statement
 #' @param x Schema to use for gluing.
 #' @inheritDotParams glue::glue_sql
+#' @export
 glue_sql.schema <- function(x, ...) glue::glue_sql(..., schema = x)
 
 #' Create scheme unless exists
@@ -25,6 +26,7 @@ create_if_not_exists <- function(x, ...) UseMethod("create_if_not_exists", x)
 #' @param .con Database connect to use for literal quoting and for executing the
 #'   schema-glued statement.
 #' @inheritDotParams DBI::dbExecute
+#' @export
 create_if_not_exists.schema <- function(x, .con = DBI::ANSI(), ...)
   DBI::dbExecute(.con,
                  glue_sql(x, "CREATE SCHEMA IF NOT EXISTS {`schema`}", .con),
