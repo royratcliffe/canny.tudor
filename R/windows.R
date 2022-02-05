42e44d5f6f927ceae58ba4b0e18a5a3fdb368cc4
Roy Ratcliffe <royratcliffe@me.com>
2022-02-05 11:26:34 +0000

    Windows.{user, system}_variables functions
    Including Windows.registry_path function.

---
+++ R/windows.R
@@ -0,0 +1,31 @@
+#' Windows User Variables
+#' @export
+#' @example
+#' Windows.user_variables()$Path
+Windows.user_variables <- function() {
+  utils::readRegistry("Environment", "HCU")
+}
+
+#' Windows System Variables
+#' @export
+#' @example
+#' Windows.system_variables()$Path
+Windows.system_variables <- function() {
+  utils::readRegistry(Windows.registry_path(
+    "SYSTEM", "CurrentControlSet", "Control",
+    "Session Manager", "Environment"
+  ), "HLM")
+}
+
+#' Windows Registry Path
+#'
+#' Constructs a Windows registry path using back-slash delimiters. The default
+#' file path separator is \emph{not} back-slash on Windows, but forward-slash
+#' instead.
+#'
+#' @param ... Registry path components, one or more.
+#' @returns Back-slash delimited registry path.
+#' @export
+Windows.registry_path <- function(...) {
+  file.path(..., fsep = "\\")
+}
