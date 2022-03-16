c9ca818a3339eaab6e3bf94d8c4e3afc3a4afbab
Roy Ratcliffe <royratcliffe@me.com>
2022-03-16 18:05:50 +0000

    fix: head class

--- R/tibble.R
+++ R/tibble.R
@@ -1,7 +1,9 @@
 #' Collect Name and Class
 #'
 #' Performs a zero-row collection on the incoming frame in order to obtain an
-#' empty \code{\link[tibble]{tibble}}.
+#' empty \code{\link[tibble]{tibble}}. Accesses only the top-most sub-class.
+#' Asking for \code{\link[base]{class}} answers a vector of classes including
+#' all super-classes.
 #'
 #' @param x Data frame to collect.
 #' @inheritDotParams tibble::tibble
@@ -10,5 +12,6 @@
 #' @export
 collect_name_and_class <- function(x, ...) {
   classes <- dplyr::collect(x, n = 0L)
-  tibble::tibble(name = names(classes), class = sapply(classes, class), ...)
+  tibble::tibble(name = names(classes), class = sapply(classes, function(x)
+    head(class(x), n = 1L)), ...)
 }
