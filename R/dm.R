edd8048aa4bb4dbfbe9e294a40f20162455940d9
Roy Ratcliffe <royratcliffe@me.com>
2022-03-16 08:33:42 +0000

    docs: more

--- R/dm.R
+++ R/dm.R
@@ -1,6 +1,9 @@
 #' Collects Classes of Table Columns
 #' @param x \code{dm} class object, a data model.
 #' @param ... Ignored parameters.
+#' @return Named list of named character vectors. The list names match the names
+#'   of the data model tables. The vector names match the names of the table
+#'   columns; the character elements describe the class of the column.
 #' @export
 collect_table_column_classes <- function(x, ...) {
   UseMethod("collect_table_column_classes")
