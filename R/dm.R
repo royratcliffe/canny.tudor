bf1fe2f36711e1ea762ecb1927ed8fdfe65cc8e6
Roy Ratcliffe <royratcliffe@me.com>
2022-03-17 12:03:16 +0000

    feat: add pk logical

--- R/dm.R
+++ R/dm.R
@@ -16,7 +16,9 @@ collect_table_column_classes.dm <- function(x, ...) {
 
 #' Collect All Column Name and Class by Table
 #'
-#' Automatically groups by table.
+#' Automatically groups by table. Adds a new logical column \code{pk} for
+#' whether or not the table-column tuple operates as part of a primary key,
+#' either alone or part of a composite primary key.
 #'
 #' @param x Data model, \code{dm} instance.
 #' @inheritDotParams purrr::imap
@@ -27,8 +29,14 @@ collect_column_classes_by_table <- function(x, ...) {
 
 #' @export
 collect_column_classes_by_table.dm <- function(x, ...) {
+  all_pks <- dm::dm_get_all_pks(dm)
+  all_pks$pk_col <- unlist(all_pks$pk_col)
   dm::dm_get_tables(x) %>%
     purrr::imap(~ collect_name_and_class(.x, table = .y), ...) %>%
     dplyr::bind_rows() %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(pk = nrow(all_pks[
+      all_pks$table == table & name == all_pks$pk_col,
+    ]) != 0L) %>%
     dplyr::group_by(table)
 }
